225954ec4a8e21a881d6559f1faf0af3d61e1b76
Monica Daza <dazaaguileramonica@gmail.com>
2017-03-01 17:41:12 +0100

    Tienda online

--- restarCantidad.php
+++ restarCantidad.php
@@ -1,20 +1,18 @@
 <?php
 session_start();
-					include "conexion.php";
-					//Agrego la variable de sesion al arreglo
-					$arreglo=$_SESSION['carrito'];
-					//Recorro el arreglo 
-					for($i=0;$i<count($arreglo);$i++){
-						//Consulto la cantidad en stcck dependiendo del Id que baya el ciclo For
-						$re=mysqli_query($con, "select stock from productos where id=".$arreglo[$i]['Id'])or die(mysqli_error($con));
-						while ($f=mysqli_fetch_array($re)) {
-							//hago el calculo de cuantos van a quedar en Stock
-							$x=$f['stock']-$arreglo[$i]['Cantidad'];
-							//Actualizo el registro stock de la BD
-							mysqli_query($con, "update productos set stock=".$x." where id=".$arreglo[$i]['Id'])or die(mysqli_error($con));
-						}
-						
-					}
+
+		include "conexion.php";
+		$arreglo=$_SESSION['carrito'];
+		
+		for($i=0;$i<count($arreglo);$i++){
+			$re=mysqli_query($con, "select stock from productos where id=".$arreglo[$i]['Id'])or die(mysqli_error($con));
+			
+			while ($f=mysqli_fetch_array($re)) {
+				$x=$f['stock']-$arreglo[$i]['Cantidad'];
+				mysqli_query($con, "update productos set stock=".$x." where id=".$arreglo[$i]['Id'])or die(mysqli_error($con));
+			}
+			
+		}
 					
 		$total=0;
 		$tabla='<table border="1"><tr>
